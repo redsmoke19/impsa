4c11afeb7a64399c60cb26b15634d9d5f195b9a1
Oleg Tsyganov <TsyganovDev@gmail.com>
2021-08-23 06:46:16 +0400

    правки

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -2,21 +2,12 @@
   'use strict';
   const historyYearSlider = new Swiper('.about-company-history__year-slider', {
     direction: 'horizontal',
-    grabCursor: true,
     preventClicks: true,
     preventClicksPropagation: true,
     slidesPerView: 'auto',
     spaceBetween: 20,
     slidesOffsetBefore: 0,
     slidesOffsetAfter: 0,
-    // breakpoints: {
-    //   768: {
-    //     slidesPerView: 2,
-    //     spaceBetween: 30,
-    //     slidesOffsetBefore: 0,
-    //     slidesOffsetAfter: 0,
-    //   },
-    // },
     // pagination: {
     //   el: '.certificates-pagination',
     //   type: 'bullets',
@@ -24,5 +15,26 @@
     //   bulletActiveClass: 'certificates-pagination__bullet--active',
     //   clickable: true,
     // },
+  });
+  const historyMainSlider = new Swiper('.about-company-history__slider', {
+    direction: 'horizontal',
+    preventClicks: true,
+    preventClicksPropagation: true,
+    slidesPerView: 'auto',
+    spaceBetween: 20,
+    slidesOffsetBefore: 0,
+    slidesOffsetAfter: 0,
+    navigation: {
+      nextEl: '.about-company-history__button--next',
+      prevEl: '.about-company-history__button--prev',
+    },
+    thumbs: {
+      swiper: historyYearSlider,
+      slideThumbActiveClass: 'about-company-history__year-item--red'
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    }
   });
 })();
